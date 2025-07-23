33ae623c17763a17498fe741926034044f758722
M4r-10 <mario.olivaspd@gmail.com>
2025-07-22 18:07:00 -0700

    setup: built structure for authentication and relevant files

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
-require("dotenv").config()
-const express = require("express")
-const cors = require("cors")
-const path = require("path")
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const authRoutes = require("./src/routes/authRoutes")
 
 const app = express();
 
@@ -9,6 +10,8 @@ app.use(cors({origin: "*", methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"], }));
 
 app.use(express.json());
+
+app.use("/api/auth", authRoutes)
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}));
 
