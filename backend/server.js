59380b647651ce86bb182507af38a9b168ebc604
M4r-10 <mario.olivaspd@gmail.com>
2025-07-22 18:36:08 -0700

    Added: provided userSchema and model for landly Users for our database

--- backend/server.js
+++ backend/server.js
@@ -15,5 +15,5 @@ app.use("/api/auth", authRoutes)
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
