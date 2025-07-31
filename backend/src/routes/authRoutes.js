3b96cdfd10f1e9b1852e18bede9d17687ac51c2d
M4r-10 <mario.olivaspd@gmail.com>
2025-07-30 20:11:54 -0700

    added authentication routes/endpoints

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,3 +1,17 @@
 const express = require("express") 
+const {registerUser, loginUser, getUserProfile} = require("../controllers/authController")
+const validation = require("../middlewares/authMiddleware")
 
 const router = express.Router()
+
+router.post("/register", registerUser)
+router.post("/login", loginUser)
+router.get("/profile", validation, getUserProfile)
+
+
+
+
+
+
+
+module.exports = router 
