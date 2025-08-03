eaa8a12cee8dac24d034e1e492fff15d3ae7b33d
M4r-10 <mario.olivaspd@gmail.com>
2025-08-02 17:35:13 -0700

    feat: added api for uploading images

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,11 +1,14 @@
-const express = require("express") 
-const {registerUser, loginUser, getUserProfile} = require("../controllers/authController")
-const validation = require("../middlewares/authMiddleware")
+const express = require("express");
+const {registerUser, loginUser, getUserProfile} = require("../controllers/authController");
+const validation = require("../middlewares/authMiddleware");
+const upload = require("../middlewares/uploadMiddleware")
+const uploadImage = require("../controllers/uploadController")
 
-const router = express.Router()
+const router = express.Router();
 
-router.post("/register", registerUser)
-router.post("/login", loginUser)
-router.get("/profile", validation, getUserProfile)
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/profile", validation, getUserProfile);
+router.post("/upload-image", upload.single("image"), uploadImage);
 
-module.exports = router 
+module.exports = router;
